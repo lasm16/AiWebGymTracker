e05346dff2981f7a7f39ae7d9cab21d436f23615
Виталя <razor52@mail.ru>
2025-05-25 20:32:56 +0300

    Добавил  модели для тренера

---
+++ AiWebGymTracker/Models/Session.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AiWebGymTracker.Models
+{
+    public class Session
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        //[Column("workout_id")]
+        //public Workout WorkoutID { get; set; }
+
+        [Column("coach_id")]
+        public Coach coach { get; set; }        
+
+        [Column("start_session")]
+        public DateTime StartSession { get; set; }
+
+        [Column("end_session")]
+        public DateTime EndSession { get; set; }
+
+        [Column("status")]
+        public SessionType  SessionType { get; set; }
+    }
+}
---
+++ AiWebGymTracker/Models/SessionType.cs
@@ -0,0 +1,9 @@
+﻿namespace AiWebGymTracker.Models
+{
+    public enum SessionType
+    {
+        Active=0,
+        Pause=1,
+        Completed=2
+    }
+}
