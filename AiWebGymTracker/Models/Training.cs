2d873f36597bf348d0973a099475fe9b8425a514
Виталя <razor52@mail.ru>
2025-05-29 00:46:13 +0300

    правка замечаний

--- AiWebGymTracker/Models/Training.cs
+++ AiWebGymTracker/Models/Training.cs
@@ -9,16 +9,16 @@ namespace AiWebGymTracker.Models
         [Column("id")]
         public int Id { get; set; }
 
-        public ApplicationUser? Coach { get; set; }
+        //public ApplicationUser? Coach { get; set; }
         public ApplicationUser Trainee { get; set; }
 
-        [Column("start_session")]
-        public DateTime StartSession { get; set; }
+        [Column("start_training")]
+        public DateTime TimeStartTraining { get; set; }
 
-        [Column("end_session")]
-        public DateTime EndSession { get; set; }
+        [Column("end_training")]
+        public DateTime TimeEndTraining { get; set; }
 
         [Column("status")]
-        public TrainingType  SessionType { get; set; }
+        public TrainingType TrainingType { get; set; }
     }
 }
