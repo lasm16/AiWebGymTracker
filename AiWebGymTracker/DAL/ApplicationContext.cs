96fcde8790b59a127ba196e9174515d86c239b02
KirSol <37743428+Xam1l1on@users.noreply.github.com>
2025-05-28 21:43:20 +0300

    Изменил название класса Meal.cs, теперь Food.cs Убрал ModelBuilder из ApplicationContext.cs Удалил класс MealType.cs Создал enum FoodCategory в классе Food.cs - содержит категорию продуктов

--- AiWebGymTracker/DAL/ApplicationContext.cs
+++ AiWebGymTracker/DAL/ApplicationContext.cs
@@ -6,32 +6,6 @@ namespace AiWebGymTracker.DAL
     public class ApplicationContext : DbContext
     {
         public DbSet<Nutrition> Nutritions { get; set; }
-        public DbSet<Meals> Meals { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Meals>().HasKey(m => m.MealId);
-            modelBuilder.Entity<Meals>()
-                .Property(m => m.Name)
-                .IsRequired()
-                .HasMaxLength(300)
-                .HasDefaultValueSql("''");
-            modelBuilder.Entity<Meals>()
-            .Property(m => m.Description)
-            .HasMaxLength(int.MaxValue)
-            .IsRequired(false);
-
-            modelBuilder.Entity<Meals>()
-                .HasMany(m => m.Ingredients)
-                .WithMany(i => i.Meals)
-                .UsingEntity<Dictionary<string, object>>(
-                    "MealIngredient",
-                    j => j.HasOne<Ingredient>().WithMany().HasForeignKey("IngredientId").HasConstraintName("FK_MealIngredient_Ingredient_IngredientId"),
-                    j => j.HasOne<Meal>().WithMany().HasForeignKey("MealId").HasConstraintName("FK_MealIngredient_Meal_MealId"),
-                    j =>
-                    {
-                        j.HasKey("MealId", "IngredientId");
-                        j.ToTable("MealIngredient");
-                    });
-        }
+        public DbSet<Food> Foods { get; set; }
     }
 }
--- AiWebGymTracker/Models/MealType.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace AiWebGymTracker.Models;
-
-public class MealType
-{
-    public int MealTypeId { get; set; }
-    public string Name { get; set; }
-}
