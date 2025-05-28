b8ea5e1ecdbcf4ea78345594b0c708ee336d798f
lasm16 <lasm16@yandex.ru>
2025-05-29 00:25:19 +0300

    hotfix

--- AiWebGymTracker/Models/Nutrition.cs
+++ AiWebGymTracker/Models/Nutrition.cs
@@ -1,9 +1,9 @@
 ﻿namespace AiWebGymTracker.Models
 {
-    public class Nutrition
+    public class Dish
     {
         public int Id { get; set; }
         //public Meal Meal { get; set; }
-        public int Count { get; set; }
+        public int Weight { get; set; }
     }
 }
