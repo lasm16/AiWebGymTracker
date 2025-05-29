6e7951c921ec5fa5924d6950d633751c419246ab
lasm16 <lasm16@yandex.ru>
2025-05-29 21:44:26 +0300

    hotfix. review

--- AiWebGymTracker/Models/Food.cs
+++ AiWebGymTracker/Models/Food.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AiWebGymTracker.Models
 {
@@ -8,22 +9,30 @@ namespace AiWebGymTracker.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(100)]
+        [Column("name")]
         public string Name { get; set; }
+        [Column("description")]
         public string Description { get; set; } = string.Empty;
         [Range(0, double.MaxValue)]
+        [Column("calories")]
         public double Calories { get; set; }
         [Range(0, double.MaxValue)]
+        [Column("protein")]
         public double Protein { get; set; }
         [Range(0, double.MaxValue)]
+        [Column("fat")]
         public double Fat { get; set; }
         [Range(0, double.MaxValue)]
+        [Column("carbohydrates")]
         public double Carbohydrates { get; set; }
         [Required]
         [Column("food_category")]
         public FoodCategory FoodCategory { get; set; }
         [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Column("updated_at")]
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        [Column("image_url")]
         public string ImageUrl { get; set; } = string.Empty;
     }
 }
