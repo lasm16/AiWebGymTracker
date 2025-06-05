f9e10fac47427e04509167bfb1ee39d42debde59
K.Solomatin <k.solomatin@agrohold.ru>
2025-06-05 11:35:21 +0300

    Добавил сущности Exercise, Workout Вспомогательный enum для Exercise

--- AiWebGymTracker/Models/Entities/Exercise.cs
+++ AiWebGymTracker/Models/Entities/Exercise.cs
@@ -1,8 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using AiWebGymTracker.Models.Enums;
 
 namespace AiWebGymTracker.Models.Entities;
 
-public class Exercise
+public class Exercise : Entity
 {
+    [Required]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ExerciseType Type { get; set; }
+    public int Repetitions { get; set; }
+    public int RangeRepetitions { get; set; }
+    public TimeSpan Duration { get; set; }
+    [Required]
+    public double Weight { get; set; }
+    public int WorkoutId { get; set; }
+    public Workout Workout { get; set; }
 
 }
