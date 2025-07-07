e0d7cd4ad0efe5632ea7658b42463508b3e92686
Xam1l1on <kir.solomatin@gmail.com>
2025-07-07 10:34:50 +0300

    Добавил атрибуты для таблицы в классе Exercise.cs. Добавил связь один ко многим Training.cs и Exercise.cs соответственно.

--- AiWebGymTracker/Models/Entities/Exercise.cs
+++ AiWebGymTracker/Models/Entities/Exercise.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using AiWebGymTracker.Models.Enums;
 
 namespace AiWebGymTracker.Models.Entities;
@@ -7,15 +8,27 @@ namespace AiWebGymTracker.Models.Entities;
 public class Exercise : Entity
 {
     [Required]
+    [Column("name")]
     public string Name { get; set; }
+    [Column("description")]
     public string Description { get; set; }
+    [Column("type")]
     public ExerciseType Type { get; set; }
+    [Range(0, int.MaxValue)]
+    [Column("repetitions")]
     public int Repetitions { get; set; }
+    [Range(0, int.MaxValue)]
+    [Column("range_repetitions")]
     public int RangeRepetitions { get; set; }
+    [Column("duration")]
     public TimeSpan Duration { get; set; }
     [Required]
+    [Column("weight")]
     public double Weight { get; set; }
-    public int WorkoutId { get; set; }
-    public Workout Workout { get; set; }
+    [Column("training_id")]
+    public int TrainingId { get; set; }
+    [ForeignKey("Training_Id")]
+    [Column("training")]
+    public Training? Training { get; set; }
 
 }
--- AiWebGymTracker/Models/Entities/Training.cs
+++ AiWebGymTracker/Models/Entities/Training.cs
@@ -20,5 +20,7 @@ namespace AiWebGymTracker.Models.Entities
 
         [Column("status")]
         public TrainingType TrainingType { get; set; }
+        [Column("exercises")]
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
     }
 }
