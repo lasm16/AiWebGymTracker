d7bac1bc7862d70830ae7e24926d9db8e59d04aa
Xam1l1on <kir.solomatin@gmail.com>
2025-07-07 10:56:50 +0300

    Убрал не асинк методы. Класс Repository стал абстракт

--- AiWebGymTracker/DAL/Repositories/Repository.cs
+++ AiWebGymTracker/DAL/Repositories/Repository.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AiWebGymTracker.DAL.Repositories;
 
-public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : class
 {
     protected readonly AppDbContext AppDbContext;
     private readonly DbSet<TEntity> _entitySet;
@@ -14,25 +14,13 @@ public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
         AppDbContext = appDbContext;
         _entitySet = appDbContext.Set<TEntity>();
     }
-    public virtual IQueryable<TEntity> GetAll()
-    {
-        return _entitySet.AsNoTracking();
-    }
     public virtual async Task<List<TEntity>> GetAllAsync()
     {
-        return await GetAll().ToListAsync();
-    }
-    public virtual TEntity Get(int id)
-    {
-        return _entitySet.Find(id);
+        return await _entitySet.ToListAsync();
     }
     public virtual async Task<TEntity> GetAsync(int id)
     {
         return await _entitySet.FindAsync(id);
-    }
-    public virtual void Insert(TEntity entity)
-    {
-        _entitySet.Add(entity);
     }
     public virtual async Task InsertAsync(TEntity entity)
     {
