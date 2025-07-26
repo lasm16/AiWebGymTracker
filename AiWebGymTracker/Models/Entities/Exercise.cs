f2695b1977c62c7e9f2a03e663d97b72d567d10c
herobeniyoutube <arseny.barinov@yahoo.com>
2025-07-26 22:10:09 +0300

    using+

--- AiWebGymTracker/Models/Entities/Exercise.cs
+++ AiWebGymTracker/Models/Entities/Exercise.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using AiWebGymTracker.Models.Abstractions;
 using AiWebGymTracker.Models.Enums;
 
 namespace AiWebGymTracker.Models.Entities;
